9e6bbdb88e819133b3f4ee74bec4c31bdb7039fe
Lovepreet singh <lovepreetsingh1223@gmail.com>
2023-10-06 13:40:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import {BrowserRouter as Router, Routes,Route} from "reaact-router-dom";
-
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
+import Header from "./components/Header";
+import Coins from "./components/Coins";
+import Exchanges from "./components/Exchange";
+import CoinDetails from "./components/CoinDetails";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-     hi
-    </div>
+   <Router>
+      <Header/>
+      <Routes>
+           <Route path="/" element={<Home/>} />
+           <Route path="/coins" element={<Coins/>} />
+           <Route path="/exchanges" element={<Exchanges/>} />
+           <Route path="/coins/:id" element={<CoinDetails/>} />
+      </Routes>
+      <Footer/>
+   </Router>
   );
 }
 
